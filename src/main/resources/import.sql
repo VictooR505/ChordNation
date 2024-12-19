f6ea53cc1441d048962988622d9ccca7e92ea7cf
hubba2010 <Hubert.Barecki@onet.pl>
2024-12-19 13:37:56 +0100

    Do exercise endpoint minor fix

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,4 @@
-insert into exercise (id, level, name, description, audio_url, points, required_points) values (1, 0, 'Chromatic - Exercise 1', 'Chromatic basics', 'url', 20, 0);
-
+-- SONGS
 insert into song (name, artist, genre, id, image_url) values ('Again', 'Flyleaf', 'ROCK', 1, 'https://i.scdn.co/image/ab67616d0000b27303716938180fa7f952eb8660');
 insert into song (name, artist, genre, id, image_url) values ('Bat Country', 'Avenged Sevenfold', 'METAL', 2, 'https://i.scdn.co/image/ab67616d0000b27391b3adb195fd4017fd3d6400');
 insert into song (name, artist, genre, id, image_url) values ('Master of Puppets', 'Metallica', 'METAL', 3, 'https://ecsmedia.pl/c/master-of-puppets-pl-b-iext127592145.jpg');
@@ -15,6 +14,7 @@ insert into song (name, artist, genre, id, image_url) values ('Cliffs of Dover',
 insert into song (name, artist, genre, id, image_url) values ('The Valley Runs Low', 'Joe Bonamassa', 'BLUES', 13, 'https://lastfm.freetls.fastly.net/i/u/500x500/e9e18f1973463297f0c54c1bddc4066f.jpg');
 insert into song (name, artist, genre, id, image_url) values ('Take Me Home, Country Roads', 'John Denver', 'COUNTRY', 14, 'https://lastfm.freetls.fastly.net/i/u/300x300/10da75dda1c1df4357280824d800f3b9.jpg');
 
+-- TABS
 insert into tab (id, song_id, level, tab_type, guitar_type, tuning, rate, rate_number, url) values (1, 1, 1, 1, 0, 0, 0, 0, 'https://example.com/tab1');
 insert into tab (id, song_id, level, tab_type, guitar_type, tuning, rate, rate_number, url) values (2, 2, 3, 1, 0, 0, 0, 0, 'https://example.com/tab1');
 insert into tab (id, song_id, level, tab_type, guitar_type, tuning, rate, rate_number, url) values (3, 3, 3, 1, 0, 0, 0, 0, 'https://example.com/tab1');
@@ -31,7 +31,35 @@ insert into tab (id, song_id, level, tab_type, guitar_type, tuning, rate, rate_n
 insert into tab (id, song_id, level, tab_type, guitar_type, tuning, rate, rate_number, url) values (14, 14, 1, 1, 0, 0, 0, 0, 'https://example.com/tab1');
 insert into tab (id, song_id, level, tab_type, guitar_type, tuning, rate, rate_number, url) values (15, 2, 1, 1, 0, 0, 0, 0, 'https://example.com/tab1');
 
-insert into users (id, email, name, guitar_type, level, points) values (1, 'test@onet.pl', 'test test 123', 1, 1, 1);
-insert into users (id, email, name, guitar_type, level, points) values (2, 'test2@onet.pl', 'test test 124', 1, 1, 2);
+-- MOCK USERS
+insert into users (id, email, name, guitar_type, level, points, first_login) values (1, 'test@onet.pl', 'test test 123', 1, 1, 1, false);
+insert into users (id, email, name, guitar_type, level, points, first_login) values (2, 'test2@onet.pl', 'test test 124', 1, 1, 2, false);
 
+-- SECTIONS
+insert into section (id, name, description) values (1, 'Sekcja 1: Wprowadzenie do Gry na Gitarze', 'Ta sekcja wprowadza podstawowe pojęcia związane z grą na gitarze, w tym podstawowe akordy i strojenie.');
+insert into section (id, name, description) values (2, 'Sekcja 2: Techniki Gry na Gitarze', 'W tej sekcji nauczysz się różnych technik gry na gitarze, takich jak użycie kostki i palcowanie.');
+insert into section (id, name, description) values (3, 'Sekcja 3: Skale i Improwizacja', 'Ta sekcja skupia się na skalach muzycznych i improwizacji na gitarze.');
+insert into section (id, name, description) values (4, 'Sekcja 4: Gra Akordów i Riffy', 'Tutaj nauczysz się przejść między akordami oraz grać proste riffy.');
+insert into section (id, name, description) values (5, 'Sekcja 5: Zaawansowane Techniki Gry', 'Ta sekcja wprowadza zaawansowane techniki gry na gitarze, takie jak bend, vibrato, slide, hammer-on i pull-off.');
 
+-- EXERCISES
+---- BEGINNER
+insert into exercise (id, level, name, description, audio_url, points, required_points, approx_duration, section_id) values (1, 0, 'Podstawowe akordy', 'Ćwiczenie 1.1: Podstawowe akordy', 'https://song1', 20, 0, '', 1);
+insert into exercise (id, level, name, description, audio_url, points, required_points, approx_duration, section_id) values (2, 0, 'Strojenie gitary', 'Ćwiczenie 1.2: Strojenie gitary', 'https://song2', 20, 0, '', 1);
+
+---- INTERMEDIATE
+insert into exercise (id, level, name, description, audio_url, points, required_points, approx_duration, section_id) values (3, 1, 'Użycie kostki', 'Ćwiczenie 2.1: Użycie kostki', 'https://song3', 25, 40, '', 2);
+insert into exercise (id, level, name, description, audio_url, points, required_points, approx_duration, section_id) values (4, 1, 'Palcowanie', 'Ćwiczenie 2.2: Palcowanie', 'https://song4', 25, 40, '', 2);
+
+---- ADVANCED
+insert into exercise (id, level, name, description, audio_url, points, required_points, approx_duration, section_id) values (5, 2, 'Skale pentatoniczne', 'Ćwiczenie 3.1: Skale pentatoniczne', 'https://song5', 30, 80, '', 3);
+
+insert into exercise (id, level, name, description, audio_url, points, required_points, approx_duration, section_id) values (6, 2, 'Improwizacja na podstawie skal', 'Ćwiczenie 3.2: Improwizacja na podstawie skal', 'https://song6', 30, 80, '', 3);
+
+insert into exercise (id, level, name, description, audio_url, points, required_points, approx_duration, section_id) values (7, 2, 'Przejścia między akordami', 'Ćwiczenie 4.1: Przejścia między akordami', 'https://song7', 25, 120, '', 4);
+
+insert into exercise (id, level, name, description, audio_url, points, required_points, approx_duration, section_id) values (8, 2, 'Granie prostych riffów', 'Ćwiczenie 4.2: Granie prostych riffów', 'https://song8', 25, 120, '', 4);
+
+---- MASTER
+insert into exercise (id, level, name, description, audio_url, points, required_points, approx_duration, section_id) values (9, 3, 'Bend i Vibrato', 'Ćwiczenie 5.1: Bend i Vibrato', 'https://song9', 35, 160, '', 5);
+insert into exercise (id, level, name, description, audio_url, points, required_points, approx_duration, section_id) values (10, 3, 'Slide i Hammer-on/Pull-off', 'Ćwiczenie 5.2: Slide i Hammer-on/Pull-off', 'https://song10', 35, 160, '', 5);
